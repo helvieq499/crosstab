0b79bd1ac96627344ce77ba0e9c53f3cf0722fe0
Quentin Helvie <73959954+helvieq499@users.noreply.github.com>
2022-03-25 09:56:11 -0400

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,18 +2,19 @@ let connections = {}
 
 onconnect = e => {
   let port = e.ports[0];
+  let ctx = {};
+    
   port.onmessage = e => {
     switch (e.data[0]) {
       case "subscribe":
         console.log(`subscribing ${e.data[1]} (${e.data[2]})`);
-        connections[port] = e.data.splice(1);
+        connections[ctx.descriptor = e.data.splice(1, 2)] = port;
         break;
       case "broadcast":
-        let current = connections[port];
-        console.log(`broadcast from ${current[0]} (${current[1]}): ${e.data[1]}`);
-        for (let p in connections) 
-          if (p != port)
-            p.postMessage(["message", "broadcast", connections[port], e.data[1]]);
+        console.log(`broadcast from ${ctx.descriptor[0]} (${ctx.descriptor[1]}): ${e.data[1]}`);
+        for (let desc in connections) 
+          if (connections[desc] != port)
+            connections[desc].postMessage(["message", "broadcast", desc, e.data[1]]);
         break;
     }
   }
