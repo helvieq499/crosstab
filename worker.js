dd1436d7b90628b557bd7d9454b0936b5e199980
Quentin Helvie <73959954+helvieq499@users.noreply.github.com>
2022-03-25 09:28:31 -0400

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,4 +1,20 @@
+let connections = {}
+
 onconnect = e => {
   let port = e.ports[0];
-  port.onmessage = e => port.postMessage(e.data);
+  port.onmessage = e => {
+    switch (e.data[0]) {
+      case "subscribe":
+        console.log(`subscribing ${e.data[1]} (${e.data[2]})`);
+        connections[port] = e.data.splice(1);
+        break;
+      case "broadcast":
+        let current = connections[port];
+        console.log(`broadcast from ${current[0]} (${current[1]}): ${e.data[1]}`);
+        for (let p in connections) 
+          if (p != port)
+            p.postMessage(["message", "broadcast", connections[port], e.data[1]]);
+        break;
+    }
+  }
 };
