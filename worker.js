034382e3ab6054db47d72a3fb9ceb8aa7531bc09
Quentin Helvie <73959954+helvieq499@users.noreply.github.com>
2022-04-22 09:16:20 -0400

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,21 +1,25 @@
 let connections = {}
 
 onconnect = e => {
-  let port = e.ports[0];
-  let ctx = {};
-    
-  port.onmessage = e => {
-    switch (e.data[0]) {
-      case "subscribe":
-        console.log(`subscribing ${e.data[1]} (${e.data[2]})`);
-        connections[ctx.descriptor = e.data.splice(1, 2)] = port;
-        break;
-      case "broadcast":
-        console.log(`broadcast from ${ctx.descriptor[0]} (${ctx.descriptor[1]}): ${e.data[1]}`);
-        for (let desc in connections) 
-          if (connections[desc] != port)
-            connections[desc].postMessage(["message", "broadcast", ctx.descriptor, e.data[1]]);
-        break;
-    }
-  }
+  let ctx = {
+    port: e.ports[0],
+  };
+
+  ctx.port.onmessage = e => handle(ctx, e.data);
 };
+
+function handle(ctx, data) {
+  switch (data[0]) {
+    case "subscribe":
+      console.log(`subscribing ${data[1]} (${data[2]})`);
+      connections[ctx.descriptor = data.splice(1, 2)] = ctx.port;
+      break;
+      
+    case "broadcast":
+      console.log(`broadcast from ${ctx.descriptor[0]} (${ctx.descriptor[1]}): ${data[1]}`);
+      for (let desc in connections) 
+        if (connections[desc] != ctx.port)
+          connections[desc].postMessage(["message", "broadcast", ctx.descriptor, data[1]]);
+      break;
+  }
+}
