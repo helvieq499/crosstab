f45891810f232813e9a77a26d4ce7c1067fab04b
Quentin Helvie <73959954+helvieq499@users.noreply.github.com>
2022-04-27 08:58:10 -0400

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,25 +1,35 @@
 let connections = {}
+let handlers = {
+  subscribe: onSubscribe,
+  broadcast: onBroadcast,
+}
 
 onconnect = e => {
   let ctx = {
     port: e.ports[0],
   };
 
-  ctx.port.onmessage = e => handle(ctx, e.data);
+  e.ports[0].onmessage = e => handle(ctx, e.data);
 };
 
 function handle(ctx, data) {
-  switch (data[0]) {
-    case "subscribe":
-      console.log(`subscribing ${data[1]} (${data[2]})`);
-      connections[ctx.descriptor = data.splice(1, 2)] = ctx.port;
-      break;
-      
-    case "broadcast":
-      console.log(`broadcast from ${ctx.descriptor[0]} (${ctx.descriptor[1]}): ${data[1]}`);
-      for (let desc in connections) 
-        if (connections[desc] != ctx.port)
-          connections[desc].postMessage(["message", "broadcast", ctx.descriptor, data[1]]);
-      break;
-  }
+  let fn = handlers[data[0]];
+  if (fn) fn(ctx, data);
+  else console.log(`unknown type ${data[0]} from ${getName(ctx)}`);
+}
+
+function onSubscribe(ctx, data) {
+  console.log(`subscribing ${data[1]} (${data[2]})`);
+  connections[ctx.descriptor = data.splice(1, 2)] = ctx.port;
+}
+
+function onBroadcast(ctx, data) {
+  console.log(`broadcast from ${getName(ctx)}: ${data[1]}`);
+  for (let desc in connections) 
+    if (connections[desc] != ctx.port)
+      connections[desc].postMessage(["message", "broadcast", ctx.descriptor, data[1]]);
+}
+
+function getName(ctx) {
+  return `${ctx.descriptor[0]} (${ctx.descriptor[1]})`;
 }
